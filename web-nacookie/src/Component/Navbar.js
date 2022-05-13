79d12f911bf24b4153db88cc6e320d945559b549
Alvin Miftah <azraqazravin@gmail.com>
2022-05-13 10:05:45 +0700

    fixing navbar

--- web-nacookie/src/Component/Navbar.js
+++ web-nacookie/src/Component/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './Navbar.css'
 import { MenuItems } from './MenuItems'
+import logo from '../assets/navbar/nacookie_logo.jpeg'
 
 function Navbar () {
 
 
   return (
     <nav className='navbar'>
-      <h1 className='navbar-logo'>NACOOKIE OAT<i className='fab fa-react'></i></h1>
+      {/* <h1 className='navbar-logo'>NACOOKIE OAT<i className='fab fa-react'></i></h1> */}
+      <img src={logo} style={{ width:'50px', margin: '4px' }}/>
       <div className='nav-menu' >
         <i></i>
       </div>
@@ -15,7 +17,8 @@ function Navbar () {
         {MenuItems.map((item, index) => {
           return (
             <li key={index}>
-              <a className={item.cName} href={item.url}>{item.title}</a></li>
+              <a className={item.cName} href={item.url}>{item.title}</a>
+            </li>
           )
         })}
       </ul>
