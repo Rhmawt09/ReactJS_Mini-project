4a914e7659a72ccab54fefd84b2953036c8d075a
Rahmawati <rhmawt09@gmail.com>
2022-05-14 10:30:05 +0700

    update navbar

--- web-nacookie/src/Component/Navbar.js
+++ web-nacookie/src/Component/Navbar.js
@@ -1,25 +1,39 @@
 import React from 'react'
 import './Navbar.css'
-import { MenuItems } from './MenuItems'
+import logo from '../assets/navbar/logo.png'
 
 function Navbar () {
-
-
   return (
-    <nav className='navbar'>
-      <h1 className='navbar-logo'>NACOOKIE OAT<i className='fab fa-react'></i></h1>
-      <div className='nav-menu' >
-        <i></i>
+    <nav className="navbar navbar-light bg-light">
+      <div className='navbar'>
+        <div className='navbar-logo'>
+           <a className="navbar-brand" href="#">
+             <img src={logo} alt="" width="30" height="24" className="d-inline-block align-text-top"/>
+                   NACOOKIE OAT
+            </a>
+       </div>
+       <div className='navbar-menu'> 
+       <div className="collapse navbar-collapse" id="navbarNav">
+         <ul className="navbar-nav">
+           <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+           </li>
+           <li className="nav-item">
+             <a className="nav-link" href="#">About Us</a>
+            </li>
+            <li className="nav-item">
+             <a className="nav-link" href="#">Our Menu</a>
+            </li>
+           <li className="nav-item">
+             <a className="nav-link" href='#'>Review</a>
+           </li>
+           <li className="nav-item">
+             <a className="nav-link" href='#'>Contact</a>
+           </li>
+          </ul>
+          </div>
+        </div>
       </div>
-      <ul>
-        {MenuItems.map((item, index) => {
-          return (
-            <li key={index}>
-              <a className={item.cName} href={item.url}>{item.title}</a></li>
-          )
-        })}
-      </ul>
-
     </nav>
   )
 }
